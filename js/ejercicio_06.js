c112d6c03c2bea1129c7e2907e104ce94e5af3eb
Luis Romero <1987romeroluisalberto@gmail.com>
2023-10-31 21:47:38 -0300

    terminado el ejercicio_05

---
+++ js/ejercicio_06.js
@@ -0,0 +1,17 @@
+/* 6- Realiza un script que escriba una pirámide del 1 al 30 de la siguiente forma :
+1
+22
+333
+4444
+55555
+666666
+…….
+ */
+
+for(let i = 1; i <= 30; i++){
+
+    for (let rep = 0; rep < i; rep++) {
+    document.write(`${i}`);
+    }
+    document.write(`<br>`)
+}
